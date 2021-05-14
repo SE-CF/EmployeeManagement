25a357f9a66d5102c7d7fbcbe7f5109afdcce3f2
MrNan <mr.nan0505@qq.com>
2021-05-14 15:25:17 +0800

    delete .umi

--- employee-frontend/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'E:/NJU/大三下/软工实验/EmployeeManagement/employee-frontend/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- employee-frontend/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'E:/NJU/大三下/软工实验/EmployeeManagement/employee-frontend/node_modules/react-helmet';
