2a2e5c784af414fbe8676e658e2a22a95b93ff57
TristanMVP <568689085@qq.com>
2021-05-24 11:33:22 +0800

    添加前端与后端的交互

--- employee-frontend/src/pages/employee/[id].jsx
+++ employee-frontend/src/pages/employee/[id].jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { connect } from 'dva'
 import ReactJson from 'react-json-view'
+import { Statistic, Row, Col, Button } from 'antd'
 
 function Employee({
   employee,
@@ -13,8 +14,9 @@ function Employee({
     dispatch({
       type: 'employee/getEmployee',
       payload: {
-        url: 'http://localhost:8000/employees/3',
+        url: 'http://localhost:8000/employees/{id}',
       },
+      id:id,
     })
   }, [])
 
@@ -22,9 +24,30 @@ function Employee({
   return (
     <div>
       <h1>Employee {id} 's Page</h1>
-      <div>
-        <ReactJson src={employee} />
-      </div>
+        <br/>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Statistic title="姓名" value={employee.name} />
+        </Col>
+      </Row>
+      <br/>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Statistic title="部门" value={employee.department}/>
+        </Col>
+        <Col span={8}>
+          <Statistic title="性别" value={employee.gender} />
+        </Col>
+      </Row>
+      <br/>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Statistic title="出生日期" value={employee.birthDate} />
+        </Col>
+        <Col span={8}>
+          <Statistic title="籍贯" value={employee.nativePlace} />
+        </Col>
+      </Row>
     </div>
   )
 }
--- employee-frontend/src/pages/employee/index.jsx
+++ employee-frontend/src/pages/employee/index.jsx
@@ -1,7 +1,19 @@
 import { useEffect } from 'react'
-import { Table, Column, Space } from 'antd'
+import { Table, Column, Space, Affix, Button } from 'antd'
 import { connect } from 'dva'
 import { Link } from 'umi'
+
+function UploadButton(){
+    return(
+        <Affix offsetTop={60} onChange={affixed => console.log(affixed)}>
+            <br/>
+            <Button>
+                <a href="/add">添加</a>
+            </Button>
+            <br/>
+        </Affix>
+    );
+}
 
 function EmployeeList({ employees, dispatch }) {
   const { Column } = Table
@@ -25,8 +37,8 @@ function EmployeeList({ employees, dispatch }) {
     },
     {
       title: 'Gender',
-      dataIndex: 'sex',
-      key: 'sex',
+      dataIndex: 'gender',
+      key: 'gender',
     },
     {
       title: 'Birth Date',
@@ -56,37 +68,41 @@ function EmployeeList({ employees, dispatch }) {
   ]
 
   return (
-    <Table dataSource={employees}>
-      <h1>Employee List Page</h1>
-
-      <Column title="ID" dataIndex="id" key="id" />
-      <Column title="Name" dataIndex="name" key="name" />
-      <Column title="Gender" dataIndex="sex" key="sex" />
-      <Column title="Birth Date" dataIndex="birthDate" key="birthDate" />
-      <Column title="Department" dataIndex="department" key="department" />
-      <Column title="Native Place" dataIndex="nativePlace" key="nativePlace" />
-      <Column
-        title="Action"
-        key="action"
-        render={(text, record) => (
-          <Space size="middle">
-            <Link to={'/employee/' + record.id}>To Employee</Link>
-            <a
-              onClick={() =>
-                dispatch({
-                  type: 'employees/deleteEmployee',
-                  payload: {
-                    id: record.id,
-                  },
-                })
-              }
-            >
-              Delete
-            </a>
-          </Space>
-        )}
-      />
-    </Table>
+    <>
+     <UploadButton/>
+     <br/>
+     <Table dataSource={employees}>
+       <h1>Employee List Page</h1>
+ 
+       <Column title="ID" dataIndex="id" key="id" />
+       <Column title="Name" dataIndex="name" key="name" />
+       <Column title="Gender" dataIndex="gender" key="gender" />
+       <Column title="Birth Date" dataIndex="birthDate" key="birthDate" />
+       <Column title="Department" dataIndex="department" key="department" />
+       <Column title="Native Place" dataIndex="nativePlace" key="nativePlace" />
+       <Column
+         title="Action"
+         key="action"
+         render={(text, record) => (
+           <Space size="middle">
+             <Link to={'/employee/' + record.id}>查看</Link>
+             <a
+               onClick={() =>
+                 dispatch({
+                   type: 'employees/deleteEmployee',
+                   payload: {
+                     id: record.id,
+                   },
+                 })
+               }
+             >
+               Delete
+             </a>
+           </Space>
+         )}
+       />
+     </Table>
+    </>
   )
 }
 
